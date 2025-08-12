f6e306103f86f0eb5dcb28c91f04e47acbe60771
AllsVeam <alaansvega@gmail.com>
2025-08-12 16:08:56 -0600

    Change to uri

--- src/main/java/org/apache/fineract/plugins/zitadel/security/service/ApiServiceImp.java
+++ src/main/java/org/apache/fineract/plugins/zitadel/security/service/ApiServiceImp.java
@@ -852,7 +852,7 @@ public class ApiServiceImp implements ApiService{
 
             String requestBody = "grant_type=authorization_code"
                     + "&code=" + URLEncoder.encode(code, StandardCharsets.UTF_8)
-                    + "&redirect_uri=" + URLEncoder.encode(frontUrl + "/callback", StandardCharsets.UTF_8)
+                    + "&redirect_uri=" + URLEncoder.encode(frontUrl + "/#/login", StandardCharsets.UTF_8)
                     + "&client_id=" + URLEncoder.encode(CLIENT_ID, StandardCharsets.UTF_8)
                     + "&code_verifier=" + URLEncoder.encode(codeVerifier, StandardCharsets.UTF_8)
                     + "&scope=" + URLEncoder.encode("openid profile email offline_access urn:zitadel:iam:org:project:321191693166617589:roles", StandardCharsets.UTF_8);
